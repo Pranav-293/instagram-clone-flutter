3e93f0273cc84b6edcee8aba26f9effe8386d960
Pranav Gupta <Pranav.19142@davietjal.org>
2022-01-24 22:47:14 +0530

    login page is to be made

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -15,10 +15,10 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SafeArea(
         child: Center(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-               Container(
-                child: const Text("Login"),
-              )
+              const SizedBox(height: 12,),
+              SvgPicture.asset("lib/assets/images/ic_instagram.svg")
             ],
           ),
         ),
